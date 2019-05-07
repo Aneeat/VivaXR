5d320adf4ede888f9ad36a5e87eb213527e5cf55
Aneeta T <aneeta.thanawalla@gmail.com>
2019-05-07 19:39:49 +0200

    Viva Videos for student platform

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,20 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.videos = functions.https.onRequest((request, response) => {
+const videos = [{ 
+    "video-id" : "01" , 
+    "video-title" : "Sagrada Familia Interior" , 
+    "video-description" : "Visit and Explore Catalan architect Antoni Gaudís Roman Catholic Church in Barcelona, Spain in honor of the holy family." ,
+    "video-URL" : "https://youtu.be/kCFWmhUWeNs" ,
+    "create-date": "05/02/2019" , 
+    "modify-date": "05/02/2019" ,
+    "long-description": "Visit and Explore Catalan architect Antoni Gaudís Roman Catholic Church in Barcelona, Spain in honor of the holy family." ,
+    "author-id": "VIVA XR" ,
+    "tag": "Travel"}
+]
+
+
+
+    response.send(videos);
+})
