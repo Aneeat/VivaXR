0b284f56c908fcdd80e4d827b722897539d9fca2
Rachel White <r0whit02@gmail.com>
2019-05-28 16:16:13 +0200

    remove duplicate footer

--- public/src/home-page.js
+++ public/src/home-page.js
@@ -94,35 +94,6 @@ class HomePage extends HTMLElement {
          </ion-col>
       </ion-row>
    </ion-grid>
-   <!--Footer start here-->
-   <div class="footer">
-      <ion-grid>
-         <ion-row>
-            <ion-col size="4">
-               <h6 class="footertext"> About us</h6>
-               <h6 class="footertext"> Explore</h6>
-               <h6 class="footertext"> Promotions</h6>
-            </ion-col>
-            <ion-col size="4">
-               <h6 class="footertext"> Upload</h6>
-               <h6 class="footertext"> Support</h6>
-               <h6 class="footertext"> FAQ's</h6>
-            </ion-col>
-            <ion-col size="4">
-               <h6 class="footertext"> Connect</h6>
-               <ion-icon name="logo-facebook"></ion-icon>
-               <ion-icon name="logo-twitter"></ion-icon>
-               <br>
-               <ion-icon name="logo-instagram"></ion-icon>
-               <ion-icon name="logo-linkedin"></ion-icon>
-            </ion-col>
-         </ion-row>
-      </ion-grid>
-   </div>
-   <ion-col class="disclaimer-text">
-      Copyright | Disclaimer | Terms & Conditions | Privacy Policy 
-   </ion-col>
-   <div></div>
 </ion-content>
 </ion-app>
 </body>
