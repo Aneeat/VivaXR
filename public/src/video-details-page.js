8fe336bbc79a8c111cc06a2558bc2ac25a17b3a9
Aneeta T <aneeta.thanawalla@gmail.com>
2019-06-10 21:13:27 +0200

    video title, alignment color of icon and video views

--- public/src/video-details-page.js
+++ public/src/video-details-page.js
@@ -3,39 +3,39 @@ class videoDetails extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = ` <ion-content>
+        this.innerHTML = ` 
+        <ion-content>
         <iframe class="iframe-container" src="https://www.youtube.com/embed/ELQh3z4KrXk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+    
         <!--Video details start here-->
-
+    
         <div class="ion-margin-start">
             <ion-grid>
-                <ion-row class="ion-align-items-center">
-                    <ion-column>
-                        <h3> Rainforests </h3>
-                    </ion-column>
-
-                    <ion-column>
-                        <ion-buttons slot="end">
-
-                            <ion-icon name="share"></ion-icon>
-                        </ion-buttons>
-                    </ion-column>
-                </ion-row>
+            <ion-row>
+            <ion-column>
+            <h1> Rainforests </h1>
+            </ion-column>
+            <ion-column>
+             <ion-buttons slot="end">
+              <ion-icon name="share" size="large" color="primary"></ion-icon>
+              </ion-buttons>
+            </ion-column>
+         </ion-row>
             </ion-grid>
-            <div class="video-views"> 142 Views </div>
-
-            <h6> Sasha Snow | 3rd July, 2019 </h6>
+            <h5> <b>142 </b> Views </h5>
+        
+    
+            <h6>  2 days ago by Sash Snow </h6>
             <div>
                 <input type="checkbox" class="read-more-state" id="expand" />
                 <p class="read-more-wrap"> This is a video description which is much longer hence Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                     <span class="read-more-target">
-           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-           </span>
+               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+               </span>
                 </p>
                 <label for="expand" class="read-more-trigger"></label>
             </div>
-
+    
             <div>
                 <h5> Similar categories </h5>
                 <div class="chip">
@@ -55,9 +55,9 @@ class videoDetails extends HTMLElement {
             </div>
             <br>
             <br>
-
+    
             <!--Similar videos start here-->
-
+    
             <h2> Similar Videos</h2>
             <ion-grid>
                 <ion-row>
@@ -67,9 +67,9 @@ class videoDetails extends HTMLElement {
                         </ion-card>
                     </ion-col>
                 </ion-row>
-
+    
                 <ion-row>
-
+    
                     <ion-col>
                         <ion-card>
                             <img src=".assets/small-4-sea-flower.png" />
@@ -84,7 +84,7 @@ class videoDetails extends HTMLElement {
             </ion-grid>
             <br>
             <br>
-
+    
             <h2> Featured Videos</h2>
             <ion-grid>
                 <ion-row>
@@ -92,17 +92,17 @@ class videoDetails extends HTMLElement {
                         <ion-card>
                             <img src="assets/bigwithtext-flyingthrough.png" />
                         </ion-card>
-
+    
                     </ion-col>
                 </ion-row>
-
+    
                 <ion-row>
                     <ion-col>
-
+    
                         <ion-card>
                             <img src="./assets/walkin-mountains-small.png" />
                         </ion-card>
-
+    
                     </ion-col>
                     <ion-col>
                         <ion-card>
@@ -111,8 +111,9 @@ class videoDetails extends HTMLElement {
                     </ion-col>
                 </ion-row>
             </ion-grid>
+        </div>
             <footer-component></footer-component>
-            </ion-content>
+    </ion-content>
             `
 
     }
